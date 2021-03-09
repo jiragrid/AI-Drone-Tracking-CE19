fb948d0c81793b9d471a8312da5548e4798474e6
Thirasan Lattesoft <earthzaa002gmail.com>
2021-03-09 14:31:19 +0700

    update COR

--- mobile-app/pages/index.js
+++ mobile-app/pages/index.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Axios from 'axios'
 
 export default function Page() {
+  const fetchData = async () => {
+    try {
+      const { data } = await Axios.get('http://0.0.0.0:5000/test')
+      console.log('fetch', data)
+    }
+    catch(error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <div>
       Hello World !!
--- restful-api/main.py
+++ restful-api/main.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS, cross_origin
 from api import TestApi
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 api.add_resource(TestApi, '/test')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
