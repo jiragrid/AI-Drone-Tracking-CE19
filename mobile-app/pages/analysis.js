cde3567ca34a309a860ca11f0e262dd34ed46c85
Thirasan Lattesoft <earthzaa002gmail.com>
2021-03-15 22:42:51 +0700

    add page analysis

---
+++ mobile-app/pages/analysis.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  Card,
+  CardContent,
+  Typography,
+} from '@material-ui/core';
+import AppContainer from '../containers/AppContainer';
+
+function Analysis() {
+  return (
+    <AppContainer>
+      <Card>
+        <CardContent>
+          Hello World
+        </CardContent>
+      </Card>
+    </AppContainer>
+  );
+};
+
+export default Analysis;
