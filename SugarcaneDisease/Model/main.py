c184773293eb4d0c18486c78967a63ac058fbf40
Thirasan Minthakhad <60010479@kmitl.ac.th>
2021-04-04 17:48:21 +0700

    update model

--- SugarcaneDisease/Model/main.py
+++ SugarcaneDisease/Model/main.py
@@ -3,8 +3,9 @@ from SugarcraneClass import SugarcaneDisease
 y_true = [0]*45 + [1]*45 + [2]*45 + [3]*45
 config_dir = './SugarcaneDisease/Model/config.json'
 logs_dir = './SugarcaneDisease/Model/logs.json'
+
 run = SugarcaneDisease(config_dir)
 # run.train()
-# run.save_log(logs_dir)
-run.test(y_true)
+result = run.test(y_true)
+run.save_log(logs_dir, result)
 
